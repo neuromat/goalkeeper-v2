4065a1893d9f19d98d19dabb50dd5aaf6180a642
Erico Silva <augusto.ericosilva@gmail.com>
2025-01-23 14:02:43 -0300

    feat: add Match scene with Transparent UI (#5)

---
+++ src/in-game/match_transparent_ui.gd
@@ -0,0 +1,58 @@
+extends Control
+
+class_name MatchTransparentUi
+
+@onready var context_tree_file_path_label: Label = $TopContainer/VBoxContainer/ContextTreeFilePath
+@onready var player_history: VBoxContainer = $TopContainer/VBoxContainer/HBoxContainer/PlayerHistory
+@onready var ai_history: VBoxContainer = $TopContainer/VBoxContainer/HBoxContainer/AiHistory
+
+# realtime labels
+@onready var readiness_label: Label = $MarginContainer2/HBoxContainer2/Readiness
+@onready var response_label: Label = $MarginContainer2/HBoxContainer2/Response
+@onready var feedback_label: Label = $MarginContainer2/HBoxContainer2/Feedback
+
+func _add_to_player_history(player_action: Array):
+	# player_action: [choice, response_time_in_us]
+	var label = Label.new()
+	label.text = "%s: %s (us)" % player_action
+	player_history.add_child(label)
+	
+func _add_to_ai_history(ai_action: Array):
+	# ai_action: [choice, context, probability]
+	var label = Label.new()
+	label.text = "%s: %s | %0.3f" % [ai_action[1], ai_action[0], ai_action[2]]
+	ai_history.add_child(label)
+
+func _update_context_tree_file_path(context_tree_file_path: String):
+	context_tree_file_path_label.text = "Context Tree: %s" \
+		% context_tree_file_path
+
+func update_realtime_timers(
+	readiness_in_s: float, response_in_us: float, feedback_in_s: float) -> void:
+	var response_in_s: float = response_in_us / pow(10, 6)
+	readiness_label.text = "Readiness (s): %0.2f" % readiness_in_s
+	response_label.text = "Response (s): %0.6f" % response_in_s
+	feedback_label.text = "Feedback (s): %0.2f" % feedback_in_s
+
+func update(player_action: Array, ai_action: Array):
+	_add_to_player_history(player_action)
+	_add_to_ai_history(ai_action)
+
+func initialize(
+	context_tree_file_path: String) -> void:
+	update_realtime_timers(0, 0, 0)
+	_update_context_tree_file_path(context_tree_file_path)
+	#_clear_player_history()
+	#_clear_ai_history()
+
+func _clear_player_history() -> void:
+	var n_children = player_history.get_child_count()
+	for i in range(1, n_children): # skip title
+		player_history.remove_child(
+			player_history.get_child(i))
+
+func _clear_ai_history() -> void:
+	var n_children = ai_history.get_child_count()
+	for i in range(1, n_children): # skip title
+		ai_history.remove_child(
+			ai_history.get_child(i))
