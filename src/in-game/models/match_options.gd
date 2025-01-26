3472580a0e7f16a2ebe62eecf3cf7e4ae831a2fb
Erico Silva <augusto.ericosilva@gmail.com>
2025-01-26 14:47:49 -0300

    fix: fix MatchOptions; add test

--- src/in-game/models/match_options.gd
+++ src/in-game/models/match_options.gd
@@ -10,7 +10,9 @@ func _init(
 	n_rounds_: int,
 	readiness_time_in_s_: float,
 	feedback_time_in_s_: float):
-	_validate(n_rounds_, readiness_time_in_s_, feedback_time_in_s_)
+	var errors = _validate(n_rounds_,
+		readiness_time_in_s_, feedback_time_in_s_)
+	assert(errors.size() == 0, 'invalid MatchOptions initialization')
 	self.context_tree_file_path = context_tree_file_path_ # TODO validate
 	self.n_rounds = n_rounds_
 	self.readiness_time_in_s = readiness_time_in_s_
@@ -19,16 +21,12 @@ func _init(
 static func _validate(n_rounds_: int,
 	readiness_time_in_s_: float,
 	feedback_time_in_s_: float,
-	testing: bool = false):
+	testing: bool = false) -> Array:
 	var errors: Array = []
 	if n_rounds_ <= 0:
 		errors.append("\n\t'n_rounds' must be > 0")
-	if readiness_time_in_s_ <= 0.0:
-		errors.append("\n\t'readiness_time_in_s' must be > 0")
+	if readiness_time_in_s_ < 0.0:
+		errors.append("\n\t'readiness_time_in_s' must be >= 0")
 	if feedback_time_in_s_ <= 0.0:
 		errors.append("\n\t'feedback_time_in_s' must be > 0")
-
-	if errors.size() > 0:
-		for e in errors:
-			push_error(e)
-		assert(testing, "invalid MatchOptions initialization")
+	return errors
