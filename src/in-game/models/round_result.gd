e9e94a670143dcf868f20355af0fc383157c8e69
Erico Silva <augusto.ericosilva@gmail.com>
2025-01-27 12:00:04 -0300

    refactor: add model for RoundResult

--- src/in-game/models/round_result.gd
+++ src/in-game/models/round_result.gd
@@ -4,5 +4,7 @@ var player_action: PlayerAction
 var ai_action: AiAction
 var winner: String
 
-func _init(player_action: PlayerAction, ai_action: AiAction) -> void:
-	pass
+func _init(player_action_: PlayerAction, ai_action_: AiAction) -> void:
+	self.player_action = player_action_
+	self.ai_action = ai_action_
+	self.winner = 'PLAYER' if player_action.choice == ai_action.choice else 'AI'
