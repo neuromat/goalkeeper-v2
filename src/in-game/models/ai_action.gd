ceb3418bff5c02eec471395dff166e95cb9af6f0
Erico Silva <augusto.ericosilva@gmail.com>
2025-01-28 14:31:11 -0300

    refactor: add to_dict to agent actions and RoundResult

--- src/in-game/models/ai_action.gd
+++ src/in-game/models/ai_action.gd
@@ -8,3 +8,13 @@ func _init(choice_: String, context_: String, probability_: float) -> void:
 	self.choice = choice_
 	self.context = context_
 	self.probability = probability_
+
+func to_dictionary() -> Dictionary:
+	return {
+		"choice": self.choice,
+		"context": self.context,
+		"probability": self.probability
+	}
+
+static func from_dictionary(dictionary: Dictionary) -> AiAction:
+	return AiAction.new(dictionary["choice"], dictionary["context"], dictionary["probability"])
--- src/in-game/models/player_action.gd
+++ src/in-game/models/player_action.gd
@@ -6,3 +6,11 @@ var response_time_in_us: int
 func _init(choice_: String, response_time_in_s_: int) -> void:
 	self.choice = choice_
 	self.response_time_in_us = response_time_in_s_
+
+
+func to_dictionary() -> Dictionary:
+	return { "choice": self.choice,
+		"response_time_in_us": self.response_time_in_us }
+
+static func from_dictionary(dictionary: Dictionary) -> PlayerAction:
+	return PlayerAction.new(dictionary["choice"], dictionary["response_time_in_us"])
