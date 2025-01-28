b408b7527c64b847819cdcfc565b48951f7f7870
Erico Silva <augusto.ericosilva@gmail.com>
2025-01-28 17:53:03 -0300

    refactor: add InGameOptions

--- src/in-game/in_game.gd
+++ src/in-game/in_game.gd
@@ -11,17 +11,15 @@ func _is_root_scene() -> bool:
 	return self == get_tree().root.get_child(0)
 
 func _play_demo():
-	var demo_options = {
-		'match_options': MatchOptions.new(
-			"res://data/01_valid_context_tree.csv",
-			3, .75, 1.25
-		)
-	}
+	var match_options = MatchOptions.new(
+			"res://data/01_valid_context_tree.csv", 3, .75, 1.25)
+	var demo_options = InGameOptions.new(match_options)
 	await start(demo_options)
 
 # public
-func start(options: Dictionary) -> void:
-	var match_result = await _play_match(options['match_options'])
+func start(in_game_options: InGameOptions) -> void:
+	var match_result = await _play_match(
+		in_game_options.match_options)
 	var in_game_event = InGameEvent.new(match_result)
 	if _is_root_scene():
 		print_debug(in_game_event.to_dictionary())
