8fe3980dda06ff828763a830ac2e0cd037256d57
Erico Silva <augusto.ericosilva@gmail.com>
2025-01-29 16:35:47 -0300

    build: Win and Linux artifacts available for downloading after CI (#14)
    * build: add step "upload artifacts"
    
    * fix: CI env var
    
    * fix: data copy from res:// to user://
    
    * build: add Win export template
    
    * build: add Win build to CI pipeline
    
    * fix: readiness label placeholder

--- src/utils/default_files_loader.gd
+++ src/utils/default_files_loader.gd
@@ -14,4 +14,21 @@ static func load():
 		if f.ends_with('.csv'):
 			var from_path = "%s/%s" % [ origin_folder_path, f ]
 			var to_path = "%s/%s" % [ destination_folder_path, f ]
-			DirAccess.copy_absolute(from_path, to_path)
+			# var error = DirAccess.copy_absolute(from_path, to_path)
+			var error = _copy_absolute(from_path, to_path)
+			if error:
+				print_debug(error)
+
+
+static func _copy_absolute(from_path: String, to_path: String) -> Error:
+	var origin_file: FileAccess = FileAccess.open(from_path, FileAccess.READ)
+	if not origin_file:
+		return FileAccess.get_open_error()
+	var content = origin_file.get_as_text()
+	origin_file.close()
+	var destination_file: FileAccess = FileAccess.open(to_path, FileAccess.WRITE)
+	if not destination_file:
+		return FileAccess.get_open_error()
+	destination_file.store_string(content)
+	destination_file.close()
+	return OK
