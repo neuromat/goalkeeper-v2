6b1e919ff0c0e1081706f2a6553437ee0d65b5fe
Erico Silva <augusto.ericosilva@gmail.com>
2025-11-05 16:06:42 -0300

    feat: parameterize assertion

--- test/unit/test_legacy_context_tree_file_parser.gd
+++ test/unit/test_legacy_context_tree_file_parser.gd
@@ -9,12 +9,13 @@ extends GutTest
 # 3. the parser acts on syntax and semantics validation
 
 var test_parameters_files_paths = [
-	'res://data/test/legacy/diamante_tree_4.txt',
+	# [ target file, length of "context and probabilities" ]
+	['res://data/test/legacy/diamante_tree_4.txt', 7]
 ]
 func test_should_validate_probability_values(
 	params=use_parameters(test_parameters_files_paths)):
 	# given
-	var file_path = params
+	var file_path = params[0]
 	
 	# when
 	var result: Dictionary = LegacyContextTreeFileParser.parse(file_path)
@@ -26,4 +27,5 @@ func test_should_validate_probability_values(
 	
 	# and
 	var contexts_and_probabilities = result['contexts_and_probabilities'] as Array
-	assert_eq(contexts_and_probabilities.size(), 7)
+	var expected_context_and_probabilities_size = params[1]
+	assert_eq(contexts_and_probabilities.size(), expected_context_and_probabilities_size)
